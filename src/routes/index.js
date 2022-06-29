afead31df62d4c140759eebd8dfddf754bdc5a3e
Jhon Light <jhony.mluz@gmail.com>
2022-06-28 21:06:19 -0300

    Alteração de rota estática

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ import user from './userRoutes.js'
 import task from './taskRoutes.js'
 
 const routes = (app) => {
-  app.route('/').get((req, res) => {
-    res.status(200).send('API rodando')
-  })
+  // app.route('/').get((req, res) => {
+  //   res.status(200).send('API rodando')
+  // })
 
   app.use(express.json(), user, task)
 }
